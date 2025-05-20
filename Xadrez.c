f34377245433d4796b6aed01f4e736be3ef46712
Passilongo <44105387+Passilongo@users.noreply.github.com>
2025-05-20 19:33:24 +0000

    Nivel mestre Xadrez concluido

--- Xadrez.c
+++ Xadrez.c
@@ -1,36 +1,28 @@
 #include <stdio.h>
 
-
-int main(){
-
-int bispo = 1, rainha = 1, cavalo = 1;
-
-// Movimentação do Bispo
-printf("\nMovimentação do Bispo\n");
-do { 
-    printf("Cima, Direita\n");
-    bispo++;
-} while (bispo <= 5);
-
-//Movimentação Rainha
-printf("\nMovimentação da Rainha\n");
-while (rainha <= 8)
-{
-    printf("Esquerda\n");
-    rainha++;
+void moverTorre(int torre){
+    if (torre > 0){
+         printf("Direita\n");
+         moverTorre(torre - 1);
+    }
 }
 
-//Movimentação da Torre
-printf("\nMovimentação da Torre\n");
-for (int i = 1; i <= 5 ; i++)
-{
-    printf("Direita\n");
+void moverRainha(int rainha){
+    if (rainha > 0){
+        printf("Esquerda \n");
+        moverRainha(rainha - 1);
+    }
 }
 
-// Movimentação do Cavalo
-printf("\nMovimentação do Cavalo\n");
-//for (cavalo = 1; cavalo <= 1; cavalo++)
-while (cavalo <= 1)
+void moverBispo(int bispo){
+    if (bispo > 0){
+        printf("Cima, Direita\n");
+        moverBispo(bispo - 1);
+    }
+}
+
+void moverCavalo(int cavalo){
+    while (cavalo < 1)
 {
 
     for (int i = 1; i <= 2; i++){
@@ -39,6 +31,54 @@ while (cavalo <= 1)
     cavalo++;
     printf("Direita \n");
 }
+}
+
+
+int main(){
+
+
+// Movimentação do Bispo
+printf("\nMovimentação do Bispo\n");
+    moverBispo(5);
+
+/*do { 
+    printf("Cima, Direita\n");
+    bispo++;
+} while (bispo <= 5);*/
+
+//Movimentação Rainha
+printf("\nMovimentação da Rainha\n");
+    moverRainha(8);
+
+/*while (rainha <= 8)
+{
+    printf("Esquerda\n");
+    rainha++;
+}*/
+
+//Movimentação da Torre
+printf("\nMovimentação da Torre\n");
+    moverTorre(5);
+
+/*for (int i = 1; i <= 5 ; i++)
+{
+    printf("Direita\n");
+}*/
+
+// Movimentação do Cavalo
+printf("\nMovimentação do Cavalo\n");
+    moverCavalo(0);
+
+//for (cavalo = 1; cavalo <= 1; cavalo++)
+/*while (cavalo <= 1)
+{
+
+    for (int i = 1; i <= 2; i++){
+        printf("Cima \n");
+    }
+    cavalo++;
+    printf("Direita \n");
+}*/
 
 
 
